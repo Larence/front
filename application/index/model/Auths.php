067f22bdfe1b8097be73f412d2ae6671edeba557
pengzhili <pengzhili@qq.com>
2019-04-30 17:15:12 +0800

    commit

---
+++ application/index/model/Auths.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/4/24
+ * Time: 17:31
+ */
+
+namespace app\index\model;
+
+
+use think\Model;
+
+class Auths extends Model
+{
+    protected $table = "auths";
+}
---
+++ application/index/model/Users.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/4/24
+ * Time: 17:32
+ */
+
+namespace app\index\model;
+
+
+use think\Model;
+
+class Users extends Model
+{
+    protected $table = "users";
+
+}
